88bbc9941913875572a45ed7d02f0470f77a816c
manuiam <m.sharma@hotmail.co.in>
2014-08-25 01:16:23 +0530

    upload of CacheMatrix Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix function defines the value of the Matrix & Inverse Matrix.
+## It also returns functions to assign Matrix, generate and assign Inverse Matrix, 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	i <- NULL				## Initialise Inverse as NULL
+	set <- function(y)	{	## Input and assign Matrix to Global Env
+		x <<- y
+		i <<- NULL			## Assign Inverse in Global Env
+	}
+	
+	get <- function()			## Returns Matrix	
+		{x}
+	
+	setinverse <- function(inverse)	## Assign Inverse Matrix
+		{i <<- inverse}
+	
+	getinverse <- function()	## Return Inverse Matrix
+		{i}
+	
+	list(set = set, 				##Return List of Functions that will be called by cacheSolve
+		get = get,
+            setinverse = setinverse,
+            getinverse = getinverse)
 }
 
+## cacheSolve takes a Matrix as argument, and checks if the Inverse is stored.
+## If inverse already calculated, will return cached value, else calculate and assigns inverse
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+	i <- x$getinverse()		
+	if(!is.null(i)) {			##Checks for Inverse value if stored
+                message("getting cached data")
+                return(i)		## Return inverse of Matrix from Cache
+	}
+	i <- solve(x$get())		## Generates and assigns inverse matrix
+	x$setinverse(i)
+      return(i)				## Return inverse of Matrix	
 }
